41d8a4543dae432a071eab9895c75f69d202ddc4
furqhanbinmoosaptr <furqhanbinmoosapr@gmail.com>
2024-05-31 13:25:07 +0530

    layout added

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
   <main className="flex h-full justify-center items-center">
-<h1 className="font-bold text-[6rem]">Hellow Quran Foundation</h1>
-
+<h2 className="font-bold">
+RUKO ZARA SABAR KARO
+</h2>
   </main>
   );
 }
