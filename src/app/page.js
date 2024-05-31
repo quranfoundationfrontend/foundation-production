762e7cc2129dc6dd04ca1ea21fb91dd5235678eb
furqhanbinmoosaptr <furqhanbinmoosapr@gmail.com>
2024-05-31 13:11:48 +0530

    Your commit message

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
   <main className="flex h-full justify-center items-center">
-<h1 className="font-bold text-[6rem]">Hellow Duniyaaaa</h1>
+<h1 className="font-bold text-[6rem]">Hellow Quran Foundation</h1>
 
   </main>
   );
