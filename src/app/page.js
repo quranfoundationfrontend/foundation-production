58abecee8a0445e4926cac3ba06e47ae7511acae
furqhanbinmoosaptr <furqhanbinmoosapr@gmail.com>
2024-05-31 12:59:11 +0530

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+  <main className="flex h-full justify-center items-center">
+<h1 className="font-bold text-[6rem]">Hellow Duniyaaaa</h1>
+
+  </main>
+  );
+}
